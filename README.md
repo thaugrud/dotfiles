989d8e19d5ec98757ce89ef352734d4b22d1020b
Thomas H <itmethomas@gmail.com>
2021-03-18 22:21:26 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# DOTFILES
+
+Installation
+
+```
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/hsamoht/dotfiles/main/setup.sh)"
+```
--- setup.sh
+++ setup.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do
-    sudo -n true
-    sleep 60
-    kill -0 "$$" || exit
-done 2>/dev/null &
-
 echo "Setting Mac settings..."
 # Enable three finger drag
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerSwipeGesture -int 1
